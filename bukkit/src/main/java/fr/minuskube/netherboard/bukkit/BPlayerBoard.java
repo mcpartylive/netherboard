a9a72006ede15e886146e1ead83c1e1e720e09f3
Lyndsey Winter <lynwin@pm.me>
2022-07-10 09:28:55 -0300

    🐛 Fix Scoreboard lines not being cleared properly

--- bukkit/src/main/java/fr/minuskube/netherboard/bukkit/BPlayerBoard.java
+++ bukkit/src/main/java/fr/minuskube/netherboard/bukkit/BPlayerBoard.java
@@ -133,7 +133,7 @@ public class BPlayerBoard implements PlayerBoard<Component, Integer, Component> 
 
         Set<Integer> scores = new HashSet<>(this.lines.keySet());
         for (int score : scores) {
-            if (score <= 0 || score > lines.length) {
+            if (score >= 0 || score > lines.length) {
                 remove(score);
             }
         }
@@ -152,7 +152,7 @@ public class BPlayerBoard implements PlayerBoard<Component, Integer, Component> 
 
         Set<Integer> scores = new HashSet<>(this.lines.keySet());
         for (int score : scores) {
-            if (score <= 0 || score > lines.size()) {
+            if (score >= 0 || score > lines.size()) {
                 remove(score);
             }
         }
